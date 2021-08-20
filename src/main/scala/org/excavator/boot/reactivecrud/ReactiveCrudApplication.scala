7bf14071398cb8d6244d92c2fc0f9b1ccde381b7
CMonkey <42.codemonkey@gmail.com>
2021-08-20 23:27:07 +0800

    add databaseInitalize

--- src/main/scala/org/excavator/boot/reactivecrud/ReactiveCrudApplication.scala
+++ src/main/scala/org/excavator/boot/reactivecrud/ReactiveCrudApplication.scala
@@ -1,10 +1,27 @@
-package org.excavator.boot.reactivecrud.entity
+package org.excavator.boot.reactivecrud
 
+import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
+import org.springframework.r2dbc.connection.init.{CompositeDatabasePopulator, ConnectionFactoryInitializer, ResourceDatabasePopulator}
 
 @SpringBootApplication
 class ReactiveCrudApplication {
+
+  @Bean
+  def databaseInitalize(connectionFactory:ConnectionFactory) = {
+    val initializer = new ConnectionFactoryInitializer
+    initializer.setConnectionFactory(connectionFactory)
+
+    val populator = new CompositeDatabasePopulator()
+    populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema/schema.sql")))
+
+    initializer.setDatabasePopulator(populator)
+
+    initializer
+  }
 
 }
 
