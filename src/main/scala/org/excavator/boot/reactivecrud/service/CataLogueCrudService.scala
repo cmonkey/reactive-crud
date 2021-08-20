ab3bfca1d53d019f8c99ad2ccd7f34ba9ff1b23c
CMonkey <42.codemonkey@gmail.com>
2021-08-20 23:28:06 +0800

    add crud function to the CataLogueCrudService

--- src/main/scala/org/excavator/boot/reactivecrud/service/CataLogueCrudService.scala
+++ src/main/scala/org/excavator/boot/reactivecrud/service/CataLogueCrudService.scala
@@ -1,5 +1,49 @@
-package org.excavator.boot.reactivecrud.controller
+package org.excavator.boot.reactivecrud.service
 
-class CataLogueCrudService {
+import org.excavator.boot.reactivecrud.entity.CataLogueItem
+import org.excavator.boot.reactivecrud.repository.CatalogueRepository
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+import java.time.Instant
+import javax.annotation.Resource
+
+@Service
+class CataLogueCrudService(@Resource catalogueRepository: CatalogueRepository) {
+  def findAll() = {
+    catalogueRepository.findAll()
+  }
+
+  def create(cataLogueItem: CataLogueItem) = {
+    catalogueRepository.save(cataLogueItem).subscribe()
+  }
+
+  def findById(id:Long) = {
+    catalogueRepository.findById(id)
+  }
+
+  def findBySku(sku:String) = {
+    catalogueRepository.findBySku(sku)
+  }
+
+  def update(cataLogueItem: CataLogueItem) = {
+    catalogueRepository.save(cataLogueItem)
+  }
+
+  def delete(id:Long) = {
+    catalogueRepository.deleteById(id)
+  }
+
+
+  def publishCatalogueItemEvent(): Unit = {
+
+  }
+
+  def addCatalogItem(cataLogueItem: CataLogueItem) = {
+    cataLogueItem.setCreatedOn(Instant.now())
+    catalogueRepository.save(cataLogueItem)
+      .doOnSuccess(item => publishCatalogueItemEvent())
+      .flatMap(item => Mono.just(item.getId))
+  }
 
 }
