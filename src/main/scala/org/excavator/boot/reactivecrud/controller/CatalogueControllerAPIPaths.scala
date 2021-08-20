b664beef2ae610bba88e3221bf507c3d6ada76bb
CMonkey <42.codemonkey@gmail.com>
2021-08-20 23:27:38 +0800

    add CatalogueControllerAPIPaths

--- src/main/scala/org/excavator/boot/reactivecrud/controller/CatalogueControllerAPIPaths.scala
+++ src/main/scala/org/excavator/boot/reactivecrud/controller/CatalogueControllerAPIPaths.scala
@@ -1,3 +1,13 @@
-package org.excavator.boot.reactivecrud.controller object CatalogueControllerAPIPaths {
+package org.excavator.boot.reactivecrud.controller
 
+object CatalogueControllerAPIPaths {
+  val CREATE = "/"
+  val GET_ITEMS = "/"
+  val GET_ITEMS_STREAM = "/stream"
+
+  val GET_ITEM = "/{sku}"
+  val UPDATE = "/{sku}"
+  val UPLOAD_IMAGE = "/{sku}/image"
+  val GET_ITEMS_WS_EVENTS = BASE_PATH + "/ws/events"
+  val BASE_PATH = "/api/v1"
 }
